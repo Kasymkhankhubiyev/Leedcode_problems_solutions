7e389d1825e90ec5135a40f1873c6835f5d40012
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-14 00:36:50 +0700

    commit triangle

--- Triangle_the_shortest_path.py
+++ Triangle_the_shortest_path.py
@@ -7,19 +7,32 @@ def minimumTotal(triangle):
     # нужна рекурсивная функция
 
     summ = triangle[0][0]
-    iterr = 0
-    for i in range(1, len(triangle)):
-        if triangle[i][iterr] > triangle[i][iterr + 1]:
-            summ += triangle[i][iterr + 1]
-            iterr = iterr + 1
-        elif triangle[i][iterr] < triangle[i][iterr + 1]:
-            summ += triangle[i][iterr]
+    if len(triangle) == 1:
+        return triangle[0][0]
+    elif len(triangle) > 2:
+        x1 = []
+        for i in range(len(triangle) - 1):
+            y1 = []
+            for j in range(i + 1):
+                y1.append(triangle[i + 1][j])
+                # print(y1)
+            x1.append(y1)
+            # print(x1)
+
+        x2 = []
+        for i in range(len(triangle) - 1):
+            y1 = []
+            for j in range(i + 1):
+                y1.append(triangle[i + 1][j + 1])
+                # print(y1)
+            x2.append(y1)
+            # print(x2)
+        if minimumTotal(x1) > minimumTotal(x2):
+            return triangle[0][0] + minimumTotal(x2)
         else:
-            if triangle[i + 1][iterr] < triangle[i + 1][iterr + 2]:
-                summ += triangle[i][iterr]
-            else:
-                summ += triangle[i][iterr + 1]
-                iterr = iterr + 1
-
-    return summ
-
+            return triangle[0][0] + minimumTotal(x1)
+    else:
+        if triangle[1][0] > triangle[1][1]:
+            return triangle[0][0] + triangle[1][1]
+        else:
+            return triangle[0][0] + triangle[1][0]
