193474d69174796e0a4cc73da890ea40512215c8
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-14 01:10:00 +0700

    commit triangle

--- Triangle_the_shortest_path.py
+++ Triangle_the_shortest_path.py
@@ -6,20 +6,28 @@ def minimumTotal(triangle):
 
     if len(triangle) == 1:
         return triangle[0][0]
+    elif check_seq(triangle, len(triangle) - 1):
+        triangle1 = triangle[:len(triangle) - 1]
+        print(len(triangle), len(triangle1))
+        return (triangle[len(triangle) - 1][0] + minimumTotal(triangle1))
     elif len(triangle) > 2:
         x1 = []
         for i in range(len(triangle) - 1):
             y1 = []
             for j in range(i + 1):
                 y1.append(triangle[i + 1][j])
+                # print(y1)
             x1.append(y1)
+            # print(x1)
 
         x2 = []
         for i in range(len(triangle) - 1):
             y1 = []
             for j in range(i + 1):
                 y1.append(triangle[i + 1][j + 1])
+                # print(y1)
             x2.append(y1)
+            # print(x2)
         min1 = minimumTotal(x1)
         min2 = minimumTotal(x2)
         if min1 > min2:
@@ -27,7 +35,20 @@ def minimumTotal(triangle):
         else:
             return triangle[0][0] + min1
     else:
-        if triangle[1][0] > triangle[1][1]:
+        if triangle[0][0] == triangle[1][0] and triangle[0][0] == triangle[1][1]:
+            return triangle[0][0] * len(triangle)
+        elif triangle[1][0] > triangle[1][1]:
             return triangle[0][0] + triangle[1][1]
         else:
             return triangle[0][0] + triangle[1][0]
+
+def check_seq(triangle, i):
+    run = True
+    for k in range(len(triangle[i])):
+        for j in range(k, len(triangle[i])):
+            if triangle[i][k] != triangle[i][j]:
+                run = False
+                break
+        if run == False:
+            break
+    return run
