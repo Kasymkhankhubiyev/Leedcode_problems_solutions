ecdcc469def93d38e17792e1efc4968cf3de5710
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-14 10:25:12 +0700

    commit triangle

--- Triangle_the_shortest_path.py
+++ Triangle_the_shortest_path.py
@@ -3,44 +3,44 @@ def minimumTotal(triangle):
     :type triangle: List[List[int]]
     :rtype: int
     """
+    memo = [[-1] * len(triangle) for _ in range(len(triangle))]
 
-    if len(triangle) == 1:
-        return triangle[0][0]
-    elif check_seq(triangle, len(triangle) - 1):
-        triangle1 = triangle[:len(triangle) - 1]
-        print(len(triangle), len(triangle1))
-        return (triangle[len(triangle) - 1][0] + minimumTotal(triangle1))
-    elif len(triangle) > 2:
-        x1 = []
-        for i in range(len(triangle) - 1):
-            y1 = []
-            for j in range(i + 1):
-                y1.append(triangle[i + 1][j])
-                # print(y1)
-            x1.append(y1)
-            # print(x1)
+    def main_body(triangle):
+        if len(triangle) == 1:
+            return triangle[0][0]
 
-        x2 = []
-        for i in range(len(triangle) - 1):
-            y1 = []
-            for j in range(i + 1):
-                y1.append(triangle[i + 1][j + 1])
-                # print(y1)
-            x2.append(y1)
-            # print(x2)
-        min1 = minimumTotal(x1)
-        min2 = minimumTotal(x2)
-        if min1 > min2:
-            return triangle[0][0] + min2
+        elif check_seq(triangle, len(triangle) - 1):
+            triangle1 = triangle[:len(triangle) - 1]
+            return (triangle[len(triangle) - 1][0] + minimumTotal(triangle1))
+
+        elif len(triangle) > 2:
+            x1 = []
+            for i in range(len(triangle) - 1):
+                y1 = []
+                for j in range(i + 1):
+                    y1.append(triangle[i + 1][j])
+                x1.append(y1)
+
+            x2 = []
+            for i in range(len(triangle) - 1):
+                y1 = []
+                for j in range(i + 1):
+                    y1.append(triangle[i + 1][j + 1])
+                x2.append(y1)
+
+            min1 = minimumTotal(x1)
+            min2 = minimumTotal(x2)
+            return triangle[0][0] + min(min1, min2)
         else:
-            return triangle[0][0] + min1
-    else:
-        if triangle[0][0] == triangle[1][0] and triangle[0][0] == triangle[1][1]:
-            return triangle[0][0] * len(triangle)
-        elif triangle[1][0] > triangle[1][1]:
-            return triangle[0][0] + triangle[1][1]
-        else:
-            return triangle[0][0] + triangle[1][0]
+            if triangle[0][0] == triangle[1][0] and triangle[0][0] == triangle[1][1]:
+                return triangle[0][0] * len(triangle)
+            else:
+                return triangle[0][0] + min(triangle[1][1], triangle[1][0])
+
+    result = main_body(triangle)
+    return result
+
+
 
 def check_seq(triangle, i):
     run = True
@@ -52,3 +52,22 @@ def check_seq(triangle, i):
         if run == False:
             break
     return run
+
+
+
+def minimumTotal_with_cache(triangle):
+    memo = [[-1] * len(triangle) for _ in range(len(triangle))] # получаем пирамуду с одинаковыми значениями
+
+    def dfs(i, j):
+        if i == len(triangle): # У ОСНОВАНИЯ
+            return 0
+        if memo[i][j] != -1: # не основание
+            return memo[i][j]
+
+        lower_left = triangle[i][j] + dfs(i + 1, j)
+        lower_right = triangle[i][j] + dfs(i + 1, j + 1)
+        memo[i][j] = min(lower_left, lower_right)
+        return memo[i][j]
+
+    return dfs(0, 0)
+
