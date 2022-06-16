d2076c553dcd96845df1794f73a5a286ae567796
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-16 13:12:46 +0700

    commit new project

---
+++ Valid_Parentheses.py
@@ -0,0 +1,21 @@
+
+def isValid(string):
+    brackets_lib = {'(': 1, ')': -1, '[': 2, ']': -2, '{': 3, '}':-3}
+    #print(items)
+
+    result = 0
+    for i in range(len(string)):
+        result += brackets_lib[string[i]]
+
+    if result == 0:
+        print('True')
+    else:
+        print('False')
+
+def isValid2(s):
+    string = s
+    for _ in range(len(string)//2):
+        if string == '':
+            return True
+        string = string.replace('()', '').replace('{}', '').replace('[]', '')
+    return string == ''
